3f9b7d5dd0fe99aebccfcdeb53f725e62a5e4d63
Josh Crawford <joshua.crawford@adelaide.edu.au>
2022-12-05 14:37:03 +0800

    Complete challenge 2

--- Day2.py
+++ Day2.py
@@ -16,6 +16,7 @@ scissorsIcons = ['C', 'Z']
 
 # Initialise score
 totalScore = 0
+realScore = 0
 
 # Begin data read
 with open(importFilename, 'r') as f:
@@ -31,43 +32,69 @@ with open(importFilename, 'r') as f:
             if play_round[1] in rockIcons:
                 # Score for playing rock
                 totalScore += rockScore
+                # Had to lose
+                realScore += 0
 
                 # Determine winnings
                 if play_round[0] in rockIcons:
                     totalScore += drawScore
+                    # Must have played scissors
+                    realScore += scissorsScore
 
                 elif play_round[0] in paperIcons:
                     totalScore += 0
+                    # Must have played rock 
+                    realScore += rockScore
 
                 elif play_round[0] in scissorsIcons:
                     totalScore += winScore
+                    # Must have played paper
+                    realScore += paperScore
 
             elif play_round[1] in paperIcons:
                 # Score for playing paper
                 totalScore += paperScore
+                # Draw score
+                realScore += drawScore
 
                 # Determine winnings
                 if play_round[0] in rockIcons:
                     totalScore += winScore
+                    # Must have played rock
+                    realScore += rockScore
 
                 elif play_round[0] in paperIcons:
                     totalScore += drawScore
+                    # Must have played paper
+                    realScore += paperScore
 
                 elif play_round[0] in scissorsIcons:
                     totalScore += 0
+                    # Must have played scissors
+                    realScore += scissorsScore
 
             elif play_round[1] in scissorsIcons:
                 # Score for playing scissors
                 totalScore += scissorsScore
+                # Winning score
+                realScore += winScore
 
                 # Determine winnings
                 if play_round[0] in rockIcons:
                     totalScore += 0
+                    # Must have played paper
+                    realScore += paperScore
 
                 elif play_round[0] in paperIcons:
                     totalScore += winScore
+                    # Must have played scissors
+                    realScore += scissorsScore
 
                 elif play_round[0] in scissorsIcons:
                     totalScore += drawScore
+                    # Must have played rock
+                    realScore += rockScore
 
 print(f"Total Score: {totalScore}")
+print(f"Real Score : {realScore}")
+
